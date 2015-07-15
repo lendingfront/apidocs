dc15eb4d87ef9efb85c8b04c05cb6056a4f2f051
lendingfront <dario@lendingfront.com>
2015-07-15 11:31:41 -0400

    Formatting changes

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 git pull origin gh-pages
 aglio -t flatly  -i apiary.apib  -o index.html
 git add .
-git commit -m "Formatting changes"
+git commit -a -m "Formatting changes"
 git push origin gh-pages
